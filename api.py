7d1eac6257d4e09cf33b908d737d2f62efc23615
20kmkim02 <kmkim2545@gmail.com>
2025-02-20 11:53:31 +0700

    All function

--- api.py
+++ api.py
@@ -13,15 +13,26 @@ Base.metadata.create_all(bind=engine)
 app =FastAPI()
 
 #---------------------------------------------------------------------------
-
-@app.post('/post_customer',response_model=ItemResponse)
-def create_Cust(item:CustomerBase,db:Session= Depends(get_db)):
-    db_item= CustomerDB(**item.model_dump())
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    print(db_item)
-    return db_item
+@app.post('/post_customer', response_model=ItemResponse)
+def create_or_update_customer(item: CustomerBase, db: Session = Depends(get_db)):
+    # หาเช็คว่าลูกค้าคนนี้เคยโผล่ในDBยัง
+    db_customer = db.query(CustomerDB).filter(CustomerDB.Telephone == item.Telephone).first()
+    
+    if db_customer:
+        # ถ้าเบอร์โทรเดิม (เคยใช้บริการมาก่อน) ก็update ข้อมูลอื่นๆของเค้า
+        for key, value in item.model_dump().items():
+            setattr(db_customer, key, value)
+        db.commit()
+        db.refresh(db_customer)
+        return {"message": "Customer updated successfully", "customer": item}
+    
+    else:
+        # Addข้อมูลลูกค้าลงDBถ้ายังไม่เคยมี
+        new_customer = CustomerDB(**item.model_dump())
+        db.add(new_customer)
+        db.commit()
+        db.refresh(new_customer)
+        return {"message": "New customer added", "customer": item}
 
 @app.get("/get_customer/{item_id}", response_model=ItemResponse)
 async def read_item(item_id: int, db: Session = Depends(get_db)):
